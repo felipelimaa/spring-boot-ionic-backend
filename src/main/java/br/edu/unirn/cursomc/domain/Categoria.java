52ce69d876492b8d2dfe92c00df20cda64eeac88
Felipe Lima <fearlimasi@gmail.com>
2018-02-10 16:00:15 -0300

    BD H2 e criação automatica da base de dados

--- src/main/java/br/edu/unirn/cursomc/domain/Categoria.java
+++ src/main/java/br/edu/unirn/cursomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package br.edu.unirn.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //entidade JPA
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //geração de chave primária
 	private Integer id;
 	private String nome;
 
