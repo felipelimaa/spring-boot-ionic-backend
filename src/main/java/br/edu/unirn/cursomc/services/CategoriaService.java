05f4b436eb52f45a5469b24c4ce33c6cb4254724
Felipe Lima <fearlimasi@gmail.com>
2018-02-11 10:38:19 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/br/edu/unirn/cursomc/services/CategoriaService.java
+++ src/main/java/br/edu/unirn/cursomc/services/CategoriaService.java
@@ -19,49 +19,54 @@ import br.edu.unirn.cursomc.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository repo; //instancia da dependencia
-	
-	public Categoria find(Integer id) { //busca de categoria
+	private CategoriaRepository repo; // instancia da dependencia
+
+	public Categoria find(Integer id) { // busca de categoria
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
-					", Tipo: " + Categoria.class.getName());
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
 		return obj;
 	}
-	
+
 	public Categoria insert(Categoria obj) {
-		obj.setId(null); //checagem se o ID está nulo, se não estiver nulo o metodo save considera como atualização
+		obj.setId(null); // checagem se o ID está nulo, se não estiver nulo o metodo save considera como
+							// atualização
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId()); //busca e verifica se existe
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId()); // busca e verifica se existe
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.delete(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	//Paginação de dados
+
+	// Paginação de dados
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-	
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+
 }
