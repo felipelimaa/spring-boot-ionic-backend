663285995958799f006e575fea045bcdbfb37906
Felipe Lima <fearlimasi@gmail.com>
2018-02-11 20:42:11 -0300

    Validação customizada: e-mail, cpf e cnpj não repetido na inserção de Cliente

--- src/main/java/br/edu/unirn/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/edu/unirn/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,17 @@ package br.edu.unirn.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.unirn.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 		
+	@Transactional(readOnly=true) //não necessita ser envolvida em uma transação no banco de dados
+	Cliente findByEmail(String email);
+	
+	@Transactional(readOnly=true) //não necessita ser envolvida em uma transação no banco de dados
+	Cliente findByCpfOuCnpj(String cpfOuCnpj);
+	
 }
--- src/main/java/br/edu/unirn/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/edu/unirn/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.unirn.cursomc.domain.Cliente;
 import br.edu.unirn.cursomc.domain.enums.TipoCliente;
 import br.edu.unirn.cursomc.dto.ClienteNewDTO;
+import br.edu.unirn.cursomc.repositories.ClienteRepository;
 import br.edu.unirn.cursomc.resources.exceptions.FieldMessage;
 import br.edu.unirn.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -21,17 +28,31 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		// inclua os testes aqui, inserindo erros na lista
-		
-		if ( objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","CPF inválido!"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido!"));
 		}
-		
-		if ( objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido!"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
 		}
-		
+
+		Cliente auxEmail = repo.findByEmail(objDto.getEmail());
+		if (auxEmail != null) {
+			list.add(new FieldMessage("email", "E-mail já existente!"));
+		}
+
+		Cliente auxCpfOuCnpj = repo.findByCpfOuCnpj(objDto.getCpfOuCnpj());
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && auxCpfOuCnpj != null) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF já existente!"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && auxCpfOuCnpj != null) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF já existente!"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
