cb7f1383b7948946ef3928be63676d983fd66148
Felipe Lima <fearlimasi@gmail.com>
2018-02-11 10:01:48 -0300

    Personalização do retorno da validação

---
+++ src/main/java/br/edu/unirn/cursomc/resources/exceptions/FieldMessage.java
@@ -0,0 +1,38 @@
+package br.edu.unirn.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+//Classe auxiliar para carregar os dados de erros
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+
+	public FieldMessage() {
+
+	}
+
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
