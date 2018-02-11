759e4384ca508e0f1ee182919539519df14f8c39
Felipe Lima <fearlimasi@gmail.com>
2018-02-10 23:24:27 -0300

    Atualização: utilizando apenas @JsonIgnore

--- src/main/java/br/edu/unirn/cursomc/domain/Pagamento.java
+++ src/main/java/br/edu/unirn/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.edu.unirn.cursomc.domain.enums.EstadoPagamento;
 
@@ -23,14 +23,13 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId //mapeamento do ID do pedido com o pagamento
 	private Pedido pedido;
 
 	public Pagamento() {
-
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
