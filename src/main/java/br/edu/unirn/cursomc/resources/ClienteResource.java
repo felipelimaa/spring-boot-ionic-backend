22cdf4f3bf7abb7b67662020bf2e7d9b6e3d2bff
Felipe Lima <fearlimasi@gmail.com>
2018-02-11 10:58:04 -0300

    Inserindo um novo Cliente com POST - Testado

--- src/main/java/br/edu/unirn/cursomc/resources/ClienteResource.java
+++ src/main/java/br/edu/unirn/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.edu.unirn.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.unirn.cursomc.domain.Cliente;
 import br.edu.unirn.cursomc.dto.ClienteDTO;
+import br.edu.unirn.cursomc.dto.ClienteNewDTO;
 import br.edu.unirn.cursomc.services.ClienteService;
 
 @RestController // controlador REST
@@ -32,7 +35,16 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody /* Converte o JSON em objeto Java */ ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// coleta a URI do novo recurso inserido
+		return ResponseEntity.created(uri).build();
+	}
+
 	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
@@ -50,17 +62,18 @@ public class ClienteResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
-		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); //stream percorre a lista
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); // stream
+																												// percorre
+																												// a
+																												// lista
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value="/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
-			@RequestParam(value="direction", defaultValue="ASC") String direction
-			) {
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
--- src/main/java/br/edu/unirn/cursomc/services/ClienteService.java
+++ src/main/java/br/edu/unirn/cursomc/services/ClienteService.java
@@ -9,9 +9,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.edu.unirn.cursomc.domain.Cidade;
 import br.edu.unirn.cursomc.domain.Cliente;
+import br.edu.unirn.cursomc.domain.Endereco;
+import br.edu.unirn.cursomc.domain.enums.TipoCliente;
 import br.edu.unirn.cursomc.dto.ClienteDTO;
+import br.edu.unirn.cursomc.dto.ClienteNewDTO;
+import br.edu.unirn.cursomc.repositories.CidadeRepository;
 import br.edu.unirn.cursomc.repositories.ClienteRepository;
+import br.edu.unirn.cursomc.repositories.EnderecoRepository;
 import br.edu.unirn.cursomc.services.exceptions.DataIntegrityException;
 import br.edu.unirn.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,6 +26,10 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo; // instancia da dependencia
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) { // busca de categoria
 		Cliente obj = repo.findOne(id);
@@ -28,6 +38,14 @@ public class ClienteService {
 					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null); // checagem se o ID está nulo, se não estiver nulo o metodo save considera como
+							// atualização
+		obj = repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		return repo.save(obj);
 	}
 
 	public Cliente update(Cliente obj) {
@@ -57,6 +75,23 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
