6f43a901d2c6c425d56d4790a64d3ba3a9191155
Felipe Lima <fearlimasi@gmail.com>
2018-02-14 12:34:47 -0300

    Expondo o header Authorization

--- src/main/java/br/edu/unirn/cursomc/resources/AuthResource.java
+++ src/main/java/br/edu/unirn/cursomc/resources/AuthResource.java
@@ -35,6 +35,7 @@ public class AuthResource {
 		String token = jwtUtil.generateToken(user.getUsername());
 		//add token no cabeçalho da requisição
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
 	
